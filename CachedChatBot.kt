dea59c4c18e544848e80c40693a0d39d48ab5069
Collin Barber <collinjbarber@gmail.com>
2023-03-03 21:21:35 -0500

    add ChatGPT api

---
+++ CachedChatBot.kt
@@ -0,0 +1,15 @@
+package me.cjcrafter.gptassist
+
+/**
+ * Caches the initial request to make interactions easier (and String based).
+ */
+class CachedChatBot(apiKey: String, public val request: ChatCompletionRequest) : ChatBot(apiKey) {
+
+    fun generateResponse(content: String, role: String = "user"): String {
+        request.messages.add(ChatMessage(role, content))
+        val response = super.generateResponse(request)
+        val temp = response.choices[0].message
+        request.messages.add(temp)
+        return temp.content
+    }
+}
